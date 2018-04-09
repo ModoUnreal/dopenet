aab6342ed28ad58015e0328f67ac162e82b8ad17
BadassBanana <alejandrohurtado888@gmail.com>
2018-04-09 15:18:03 +0100

    Added models and routes.py files

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Welcome to dopenet!!!'
+    username = "ModoUnreal"
+    return render_template('base.html', title='Dopenet: You can do anything', username=username)
