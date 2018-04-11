52e640d29df71e365cd6e70c2cd1c47e94a541c9
BadassBanana <alejandrohurtado888@gmail.com>
2018-04-11 08:27:09 +0100

    Added registration and other stuff

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,58 @@
-from flask import render_template
-from app.forms import LoginForm
-from app import app
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import logout_user, current_user, login_user, login_required
+from werkzeug.urls import url_parse
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from app import app, db
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'ModoUnreal'}
+    return render_template('index.html', title='Dopenet: You can do anything', )
 
-    posts = [
-            
-            {
-                'author': {'username': 'Captain Swiggles'},
-                'body': 'Hey this is me, just checkin out..'
-            }
-            
-            ]
-
-    return render_template('index.html', title='Dopenet: You can do anything', user=user, posts=posts)
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    loginform = LoginForm()
-    return render_template('login.html', title='Login to Dopenet', form=loginform)
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Login to Dopenet', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Congrats!!! You are now registered to Dopenet!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/submit')
+def submit():
+    return "This will be used to submit stuff"
