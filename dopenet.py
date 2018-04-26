cbf859532029388c7836f70741f190fa2b9886b2
Riccardo Berto <riccardobrt@gmail.com>
2018-04-26 08:48:11 +0200

    Deployment with waitress, rm of __pycache__ dirs

--- dopenet.py
+++ dopenet.py
@@ -1,6 +1,10 @@
 from app import app, db
 from app.models import User, Post, Comment
+from waitress import serve
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Comment': Comment}
+
+if __name__ == '__main__':
+    serve(app, host='127.0.0.1', port=5000)
